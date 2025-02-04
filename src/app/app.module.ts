5a7f3e67cff57850c0abd151f1dbb6056c8f6e44
Eva <elopezmarquez@safareyes.es>
2025-02-04 23:44:55 +0100

    angular-firestore importados en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { environment } from '../environments/environment';  // Importa la configuración de Firebase
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Importa Firebase y módulos de AngularFire
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    AngularFireAuthModule,  // Para autenticación
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
